e9868cc31893f3dd862eb7ff87acb10db2e91612
Lisa <lisa.exz@gmail.com>
2019-06-14 14:32:30 +0300

    add library 'readline-sync'

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "source-map-support/register";
+import readlineSync from "readline-sync";
+
+export default () => {
+  const userName = readlineSync.question("May I have you name? ");
+  console.log(`Hello, ${userName}!`);
+};
